429895eb2a4de9460949e4021bff5ea333441a4d
simpart <simpart>
2019-06-16 23:53:33 +0000

    change select

--- index.js
+++ index.js
@@ -1,88 +1,74 @@
 /**
- * @file dropdown.js
+ * @file   mofron-comp-dropdown/index.js
+ * @brief  dropdown component for mofron
  * @author simpart
  */
-let mf       = require('mofron');
-let Text     = require('mofron-comp-text');
-let FormItem = require('mofron-comp-formitem');
-let eCom     = require('mofron-event-common');
+const mf       = require("mofron");
+const FormItem = require("mofron-comp-formitem");
+const Text     = require("mofron-comp-text");
+const evCommon = require("mofron-event-oncommon");
 
-/**
- * @class mofron.comp.DropDown
- * @brief DropDown Component class
- */
 mf.comp.DropDown = class extends FormItem {
     
     /**
-     * initialize inputtext component
+     * initialize dropdown component
      * 
-     * @param po : (array) contents
-     * @param po : (object) option
+     * @param 'text' parameter
+     * @type private
      */
-    constructor (po, lst, idx) {
+    constructor (po) {
         try {
             super();
-            this.name('DropDown');
-            this.prmOpt(po, lst, idx);
+            this.name("DropDown");
+            this.prmMap("text");
+            this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
+    
     /**
-     * initialize DOM contents
+     * initialize dom contents
      * 
-     * @param prm (object) : Date object
+     * @type private
      */
-    initDomConts (lbl, lst, idx) {
+    initDomConts () {
         try {
-            super.initDomConts(lbl);
-            let sel = new mf.Dom('select',this);
+            super.initDomConts();
+            let sel = new mf.Dom("select", this);
             this.target().addChild(sel);
             this.target(sel);
-            this.styleTgt(sel);
-            
-            /* set default */
-            if (undefined !== lst) { 
-                this.addList(lst);
-                this.select((undefined === idx) ? 0 : idx);
-            }
             
             /* init change event */
-            let evt_fnc = (dd_obj) => {
+            let chg_evt = (p1,p2,p3) => {
                 try {
-                    let chg_evt = dd_obj.changeEvent();
-                    if (null !== chg_evt) {
-                        for (let eidx in chg_evt) {
-                            chg_evt[eidx][0](dd_obj, chg_evt[eidx][1]);
-                        }
+                    let cbx_evt = p1.changeEvent();
+                    for (let cb_idx in cbx_evt) {
+                        cbx_evt[cb_idx][0](p1, p1.select(), cbx_evt[cb_idx][1]);
                     }
                 } catch (e) {
                     console.error(e.stack);
                     throw e;
                 }
-            };
-            this.addEvent(
-                new eCom({
-                    handler   : new mf.Param(evt_fnc, this),
-                    eventName : 'onchange'
-                })
-            );
+            }
+            this.event(new evCommon(chg_evt, "onchange"));
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    afterRender () {
+    /**
+     * set select index
+     *
+     * @type private 
+     */
+    beforeRender () {
         try {
-            super.afterRender();
-            let chd = this.child();
-            for (let cidx in chd) {
-                if (null !== chd[cidx].target().attr('selected')) {
-                    this.target().getRawDom().selectedIndex = parseInt(cidx);
-                    break;
-                }
+            super.beforeRender();
+            if (null !== this.select()) {
+                this.select(this.select());
             }
         } catch (e) {
             console.error(e.stack);
@@ -90,180 +76,109 @@ mf.comp.DropDown = class extends FormItem {
         }
     }
     
-    addChild (chd, idx, flg) {
-        try {
-            if (false === flg) {
-                super.addChild(chd, idx);
-                return;
-            }
-            
-            if (null !== this.height()) {
-                if (true === mf.func.isInclude(chd, 'Text')) {
-                    chd.size(this.height());
-                }
-            }
-            if (0 !== this.getChild(true).length) {
-                chd.target().tag('option');
-            }
-            super.addChild(chd, idx);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    size (x, y) {
-        try {
-            let ret = super.size(x, y);
-            if (undefined !== y) {
-                /* setter */
-                this.label().size(y);
-                let chd = this.child();
-                for (let idx in chd) {
-                    if (true === mf.func.isInclude(chd[idx], 'Text')) {
-                        chd[idx].size(y);
-                    } else {
-                        chd[idx].height(y);
-                    }
-                }
-            }
-            return ret;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    select (idx) {
-        try {
-            if (undefined === idx) {
-                /* getter */
-                var ret_val = new Array();
-                for (var loop=0; loop < this.child().length ;loop++) {
-                    if (true === this.isSelect(loop)) {
-                        return loop;
-                    }
-                }
-                return ret_val;
-            }
-            /* setter */
-            this.adom();
-            if ('string' === typeof idx) {
-                idx = parseInt(idx);
-            }
-            let chd = this.child();
-            if (undefined === chd[idx]) {
-                throw new Error('invalid parameter');
-            }
-            
-            /* clear selected */
-            for (let cidx in chd) {
-                chd[cidx].target().attr('selected', null);
-            }
-            if (true === this.target().isPushed()) {
-                this.target().getRawDom().selectedIndex = idx;
-            } else {
-                chd[idx].target().attr('selected', "");
-            }
-            
-            /* execute change event */
-            let chg_evt = this.changeEvent();
-            if (null !== chg_evt) {
-                for (let eidx in chg_evt) {
-                    chg_evt[eidx][0](this, chg_evt[eidx][1]);
-                }
-            }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    isSelect (idx) {
-        try {
-            var chd = this.child();
-            if ('string' === typeof idx) {
-                idx = parseInt(idx);
-            }
-            
-            if (undefined === chd[idx]) {
-                throw new Error('invalid parameter');
-            }
-            
-            if (true === this.target().isPushed()) {
-                return (this.target().getRawDom().selectedIndex === idx) ? true : false;
-            } else {
-                var ret_val = chd[idx].target().prop('selected');
-                return (null === ret_val) ? false : ret_val;
-            }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    value (prm) {
+    /**
+     * select text list
+     *
+     * @param (string/array) select text contents
+     * @return (array) select text contents
+     * @type tag parameter
+     */
+    text (prm) {
         try {
             if (undefined === prm) {
-                /* getter*/
-                return this.select();
+                /* getter */
+                let ret = [];
+                let chd = this.target().child();
+                for (let cidx in chd) {
+                    ret.push(chd[cidx].text);
+                }
+                return (0 === ret.length) ? null : ret;
             }
             /* setter */
-            this.select(prm);
+            if (true === Array.isArray(prm)) {
+                for (let pidx in prm) {
+                    this.text(prm[pidx]);
+                }
+                return;
+            }
+            if ("string" === typeof prm) {
+                this.target().addChild(
+                    new mf.Dom({
+                        tag: "option", component: this, text: prm
+                    })
+                );
+            } else {
+                throw new Error("invalid parameter");
+            }
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    addList (prm) {
+    /**
+     * item value
+     *
+     * @param (number) selected index
+     * @return (number) selected index
+     * @type function
+     */
+    value (prm, flg) {
         try {
-            if (true !== Array.isArray(prm)) {
-                throw new Error('invalid parameter');
+            let opts = this.target().child();
+            if (undefined === prm) {
+                if (false === this.adom().isPushed()) {
+                    return this.member("value", "number");
+                }
+                /* getter */
+                for (let oidx in opts) {
+                    if (true === opts[oidx].prop("selected")) {
+                        return parseInt(oidx);
+                    }
+                }
+                return null;
             }
-            for (let pidx in prm) {
-                if ('string' === typeof prm[pidx]) {
-                    this.addChild(new Text(prm[pidx]));
-                }else if (true === mf.func.isInclude(prm[pidx], 'Component')) {
-                    this.addChild(prm[pidx]);
+            /* setter */
+            if ( (undefined === opts[prm]) && (true !== flg) ) {
+                if (false === this.adom().isPushed()) {
+                    this.member("value", "number", prm);
+                    return;
                 } else {
-                    throw new Error('invalid parameter');
+                    throw new Error("invalid parameter");
                 }
             }
+            opts[prm].prop("selected", true);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    color (prm) {
-        try {
-            let ret = super.color(prm);
-            if (undefined === ret) {
-                /* setter */
-                let rgb = prm.rgba();
-                rgb[0] = (0 > (rgb[0]-30)) ? 0 : rgb[0]-30;
-                rgb[1] = (0 > (rgb[1]-30)) ? 0 : rgb[1]-30;
-                rgb[2] = (0 > (rgb[2]-30)) ? 0 : rgb[2]-30;
-                this.style({
-                    'border-color' : new mf.Color(rgb[0], rgb[1], rgb[2]).getStyle()
-                });
-            }
-            return ret;
-        } catch (e) {
+    /**
+     * item value
+     *
+     * @param (number) selected index
+     * @return (number) selected index
+     * @type tag parameter
+     */
+    select (prm) {
+        try { return this.value(prm); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * clear item value
+     *
+     * @type function
+     */
     clear () {
-        try {
-            this.select(0);
-        } catch (e) {
+        try { this.select(0); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.comp.DropDown;
+module.exports = mf.comp.DropDown;
 /* end of file */
